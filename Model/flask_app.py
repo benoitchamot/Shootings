d1437da42ed08dbf2d999b1dcc40db12fa53296f
Benoit Chamot <benoit.chamot@gmail.com>
2023-11-05 17:21:30 +1100

    Complete MVP for OpenBox

---
+++ Model/flask_app.py
@@ -0,0 +1,87 @@
+#########################################################
+# Dependencies
+#########################################################
+from flask import Flask, jsonify
+import numpy as np
+import pandas as pd
+import datetime as dt
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func, desc
+from pathlib import Path
+
+# CORS
+from flask_cors import CORS
+
+# Ignore warnings
+import warnings
+warnings.simplefilter(action='ignore')
+
+#########################################################
+# Database Setup
+#########################################################
+
+# Load data from CSV file into pandas DataFrame
+csv = Path('model_openbox_risks.csv')
+risk_df = pd.read_csv(csv)
+
+#########################################################
+# Functions
+#########################################################
+
+# Determine risk
+def get_risk(risk_df, state, age_bracket, mental_illness, employment, arrest, autism):
+    # Filter based on state
+    filtered_df = risk_df.loc[risk_df['State']==state,:]
+
+    risk_age = filtered_df[age_bracket].values[0]
+
+    risk_mental = 1
+    risk_employment = 1
+    risk_arrest = 1
+    risk_autism = 1
+
+    if mental_illness == '1':
+        risk_mental = filtered_df['Mental_Illness_Risk'].values[0]
+    elif mental_illness == '0':
+        risk_mental = 1
+
+    if employment == '1':
+        risk_employment = 1
+    elif employment == '0':
+        risk_employment = filtered_df['Unemployment_Risk'].values[0]
+
+    if arrest == '1':
+        risk_arrest = filtered_df['Arrest_Risk'].values[0]
+    elif arrest == '1':
+        risk_arrest = 1
+
+    if autism == '1':
+        risk_autism = filtered_df['Autism_Risk'].values[0]
+    elif autism == '1':
+        risk_autism = 1
+
+    return [risk_age, risk_mental, risk_employment, risk_arrest, risk_autism]
+
+#########################################################
+# Flask Setup
+#########################################################
+app = Flask(__name__)
+CORS(app)
+
+#########################################################
+# Flask Dynamic Routes
+#########################################################
+
+@app.route("/api/v1.0/openbox/<state>/<age_bracket>/<mental_illness>/<employment>/<arrest>/<autism>")
+def api_openbox(state, age_bracket, mental_illness, employment, arrest, autism):
+
+    total_tisk = get_risk(risk_df, state, age_bracket, mental_illness, employment, arrest, autism)
+
+    return jsonify({'Risk': total_tisk})
+
+#########################################################
+# Run App
+#########################################################
+if __name__ == "__main__":
+    app.run(debug=True)
--- docs/static/js/openbox.js
+++ docs/static/js/openbox.js
@@ -1,5 +1,5 @@
-let state = '';
-let age_bracket = '';
+let state = 'AK';
+let age_bracket = 'Under 10 years old';
 let mental_illness = 0;
 let employment = 0;
 let arrest = 0;
@@ -7,38 +7,63 @@ let autism = 0;
 
 let risk = 1;
 
-function get_state(value) {
-    console.log(value);
-    state = value;
+function displayRisk(data) {
+    let total_risk = data.Risk[0]*data.Risk[1]*data.Risk[2]*data.Risk[3]*data.Risk[4]
+    console.log(total_risk);
 
+    if (age_bracket == 'Under 10 years old' || age_bracket == '10 to 14 years' || age_bracket == '15 to 17 years') {
+        d3.select('#risk_category').text("Not allowed")
+        d3.select('#risk_value').text("Prospect is too young to have a gun.");
+    }
+    else {
+        d3.select('#risk_value').text(total_risk);
+    }
+
+    if (total_risk < 10) {
+        d3.select('#risk_category').text("Risk: Low")
+    }
+    else if (total_risk < 100) {
+        d3.select('#risk_category').text("Risk: Medium")
+    }
+    else if (total_risk < 1000) {
+        d3.select('#risk_category').text("Risk: High")
+    }
+    else {
+        d3.select('#risk_category').text("Risk: Very High")
+    }
+}
+
+function calculate_risk() {
+    risk_url = 'http://127.0.0.1:5000/api/v1.0/openbox/' + state + '/' + age_bracket + '/' + mental_illness + '/' + employment + '/' + arrest + '/' + autism;
+    d3.json(risk_url).then(displayRisk);
+}
+
+function get_state(value) {
+    state = value;
+    calculate_risk();
 }
 
 function get_age(value) {
-    console.log(value);
     age_bracket = value;
-
+    calculate_risk();
 }
 
 function check_mental(value) {
-    console.log(value);
     mental_illness = value;
-
+    calculate_risk();
 }
 
 function check_employment(value) {
-    console.log(value);
     employment = value;
-
+    calculate_risk();
 }
 
 function check_arrested(value) {
-    console.log(value);
     arrest = value;
-
+    calculate_risk();
 }
 
 function check_autism(value) {
-    console.log(value);
     autism = value;
-
+    calculate_risk();
 }
