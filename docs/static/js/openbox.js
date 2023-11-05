50238c1f038f2cbe5cc6b3914569467589addfe5
Benoit Chamot <benoit.chamot@gmail.com>
2023-11-05 16:09:41 +1100

    Prepare skeleton web app for OpenBox model.

---
+++ docs/static/js/openbox.js
@@ -0,0 +1,44 @@
+let state = '';
+let age_bracket = '';
+let mental_illness = 0;
+let employment = 0;
+let arrest = 0;
+let autism = 0;
+
+let risk = 1;
+
+function get_state(value) {
+    console.log(value);
+    state = value;
+
+}
+
+function get_age(value) {
+    console.log(value);
+    age_bracket = value;
+
+}
+
+function check_mental(value) {
+    console.log(value);
+    mental_illness = value;
+
+}
+
+function check_employment(value) {
+    console.log(value);
+    employment = value;
+
+}
+
+function check_arrested(value) {
+    console.log(value);
+    arrest = value;
+
+}
+
+function check_autism(value) {
+    console.log(value);
+    autism = value;
+
+}
