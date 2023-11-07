ec5c9bba57dba5c9b7c3981224fe20362de6eb1b
Benoit Chamot <benoit.chamot@gmail.com>
2023-11-07 16:52:38 +1100

    Fix bug in module (non-notebook) version of BlackBox. Infrastructure complete to move data and classification back and forth.

--- Model/flask_app.py
+++ Model/flask_app.py
@@ -9,7 +9,6 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func, desc
 from pathlib import Path
-import pickle
 
 # CORS
 from flask_cors import CORS
@@ -17,6 +16,9 @@ from flask_cors import CORS
 # Ignore warnings
 import warnings
 warnings.simplefilter(action='ignore')
+
+# Import local modules
+from model_blacbox import predict_with_blackbox
 
 #########################################################
 # Database Setup
@@ -89,95 +91,12 @@ def api_openbox(state, age_bracket, mental_illness, employment, arrest, autism):
 @app.route("/api/v1.0/blackbox/<id>/<Age>/<Gender>/<Race>/<Immigrant>/<Education>/<RelStatus>/<Employed>/<Work>/<MilService>/<Arrested>/<ParentDivorce>/<SES>/<MentalIllness>/<MentalIllnessHistory>/<Autism>/<HealthIssues>")
 def api_blackbox(id, Age, Gender, Race, Immigrant, Education, RelStatus, Employed, Work, MilService, Arrested, ParentDivorce, SES, MentalIllness, MentalIllnessHistory, Autism, HealthIssues):
 
-    # Create a DataFrame to hold the data
-    input_data = pd.DataFrame([{
-        'Age': Age,
-        'Gender': Gender,
-        'Race': Race,
-        'Immigrant': Immigrant,
-        'Education': Education.replace("+", "/"),
-        'RelStatus': RelStatus.replace("+", "/"),
-        'Employed': Employed,
-        'Work': Work,
-        'MilService': MilService,
-        'Arrested': Arrested,
-        'ParentDivorce': ParentDivorce,
-        'SES': SES,
-        'MentalIllness': MentalIllness,
-        'MentalIllnessHistory': MentalIllnessHistory,
-        'Autism': Autism,
-        'HealthIssues': HealthIssues
-    }])
-
-    # Get dummies
-    dummies_df = pd.get_dummies(input_data.drop(columns=['Age']))
-    X = pd.concat([input_data['Age'], dummies_df], axis=1)
-
-    # Get features from dataset
-    dataset_features = X.columns
-
-    # Load model from file
-    with open('blackbox.model','rb') as f:
-        model_1 = pickle.load(f)
-
-    # Get names of necessary features
-    model_features = model_1.feature_names_in_
-
-    # Create empty list to store missing features
-    missing_features = []
-
-    # Create empty list to store extra features
-    extra_features = []
-
-    # Check if the dataset is missing features
-    if len(dataset_features) < len(model_features):
-
-        # Loop through model features
-        for feature in model_features:
-            # Check if feature is missing and add missing feature to the list
-            if feature not in dataset_features:
-                missing_features.append(feature)
-
-    # Check if the dataset has too many features
-    elif len(dataset_features) > len(model_features):
-
-        # Loop through dataset features
-        for feature in dataset_features:
-            # Check if feature is missing and add missing feature to the list
-            if feature not in model_features:
-                extra_features.append(feature)
-
-    # If the number of features is the same, make sure they are identical
-    if len(dataset_features) == len(model_features):
-        
-        # Loop through dataset features
-        for feature in dataset_features:
-            # Check if feature is missing and add missing feature to the list
-            if feature not in model_features:
-                extra_features.append(feature)
-
-        # Loop through model features
-        for feature in model_features:
-            # Check if feature is missing and add missing feature to the list
-            if feature not in dataset_features:
-                missing_features.append(feature)
-
-
-    # Add missing features to dataset with value of 0
-    for mf in missing_features:
-        X[mf] = 0
-
-    # Drop extra features from DataSet
-    X = X.drop(columns=extra_features)
-    
-    # Data Preparation
-    X = X.values
+    classification, probability = predict_with_blackbox(Age, Gender, Race, Immigrant, Education, RelStatus, Employed, Work, MilService, Arrested, ParentDivorce, SES, MentalIllness, MentalIllnessHistory, Autism, HealthIssues)
 
     # Return classification (1 or 0) and probability of individual of being classified 1
     return jsonify({'ID': id,
-                    'Classification': model_1.predict(X).tolist(),
-                    'Probability': 100*model_1.predict_proba(X)[0,1].tolist()})
-
+                    'Classification': classification,
+                    'Probability': probability})
 
 #########################################################
 # Run App
